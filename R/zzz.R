80771aec04797ef354ea3d9e27516bb25dcdd3ec
Torben <torben.kimhofer@murdoch.edu.au>
2020-01-08 17:13:26 +0800

    upd web

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 # add www folder to resource path
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to the MSbrowser! If you encounter any issues or have queries please visit https://github.com/tkimhofer/msbrowser/issues or send a report to msbrowser@tkimhofer.com.")
+  packageStartupMessage("Welcome to the MSbrowser! If you encounter any issues or have queries please log @ https://github.com/tkimhofer/msbrowser/issues.")
   addResourcePath('www',  system.file("www", package = "msbrowser"))
 
   invisible(suppressPackageStartupMessages(
