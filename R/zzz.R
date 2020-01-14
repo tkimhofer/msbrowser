ea7302d0646116c86551fd68a4580b53ba413a88
Torben <torben.kimhofer@murdoch.edu.au>
2020-01-14 19:34:28 +0800

    st upd

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 # add www folder to resource path
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Welcome to the MSbrowser! If you encounter any issues or have queries please log @ https://github.com/tkimhofer/msbrowser/issues.")
-  addResourcePath('www',  system.file("www", package = "msbrowser"))
+  #addResourcePath('www',  system.file("www", package = "msbrowser"))
 
   invisible(suppressPackageStartupMessages(
     sapply(c('xcms', 'mzR', 'methods', 'shiny'),
